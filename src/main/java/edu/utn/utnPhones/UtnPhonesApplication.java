4f2f5ad3dc22272933a7e5d38914d872e3a99a89
FedeAlesandro <fede.alesandro@gmail.com>
2020-05-08 23:45:56 -0300

    [Created] New project made

---
+++ src/main/java/edu/utn/utnPhones/UtnPhonesApplication.java
@@ -0,0 +1,13 @@
+package edu.utn.utnPhones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtnPhonesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtnPhonesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/utn/utnPhones/UtnPhonesApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.utn.utnPhones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtnPhonesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
