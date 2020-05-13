aac656cdcdf9f543c364ade50cccd1e0319d654e
Fabio Laguna <fabiolaguna.94@gmail.com>
2020-05-13 17:56:57 -0300

    [Update] Enum and idPhoneLines in PhoneCall class added

--- src/main/java/edu/utn/utnPhones/model/Bill.java
+++ src/main/java/edu/utn/utnPhones/model/Bill.java
@@ -1,16 +1,54 @@
 package edu.utn.utnPhones.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class Bill {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer idBill;
+
+    @NotNull
     private PhoneLine phoneLine;
+
+    @NotNull
     private Integer callsAmount;
+
+    @NotNull
     private BigDecimal totalCost;
+
+    @NotNull
     private BigDecimal totalPrice;
+
+    @NotNull
     private LocalDateTime billDate;
+
+    @NotNull
     private LocalDateTime billDateExpiration;
-    private String state;
+
+    public enum BillStatus {
+
+        payed, expired
+    }
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private BillStatus state;
+
 }
--- src/main/java/edu/utn/utnPhones/model/Tariff.java
+++ src/main/java/edu/utn/utnPhones/model/Tariff.java
@@ -1,11 +1,35 @@
 package edu.utn.utnPhones.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class Tariff {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer idTariff;
+
+    @NotNull
     private City originCity;
+
+    @NotNull
     private City destinationCity;
+
+    @NotNull
     private Float pricePerMinute;
+
+    @NotNull
     private Float costPerMinute;
 
 }
