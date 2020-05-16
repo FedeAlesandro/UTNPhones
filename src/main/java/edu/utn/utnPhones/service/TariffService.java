a570b305e736adb95b6c8442a426255965e97f2c
Fabio Laguna <fabiolaguna.94@gmail.com>
2020-05-16 13:27:37 -0300

    [Create] Bill - Tariff - PhoneCall  controllers, services and repositories

---
+++ src/main/java/edu/utn/utnPhones/service/TariffService.java
@@ -0,0 +1,27 @@
+package edu.utn.utnPhones.service;
+
+import edu.utn.utnPhones.dao.TariffRepository;
+import edu.utn.utnPhones.model.Tariff;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TariffService {
+
+    private TariffRepository tariffRepository;
+
+    @Autowired
+    public TariffService(TariffRepository tariffRepository){
+        this.tariffRepository = tariffRepository;
+    }
+
+    public List<Tariff> getTariffs() {
+        return tariffRepository.findAll();
+    }
+
+    public void addTariff(Tariff tariff) {
+        tariffRepository.save(tariff);
+    }
+}
