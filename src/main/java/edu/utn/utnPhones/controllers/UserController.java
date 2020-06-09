35bbd050ca497d56e84265256fa13acda30fd046
FedeAlesandro <fede.alesandro@gmail.com>
2020-06-09 00:26:59 -0300

    [Feature] Web controller created

--- src/main/java/edu/utn/utnPhones/controllers/UserController.java
+++ src/main/java/edu/utn/utnPhones/controllers/UserController.java
@@ -13,6 +13,7 @@ import edu.utn.utnPhones.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -20,15 +21,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
 
-@RestController
-@RequestMapping("/users")
+@Controller
 public class UserController {
 
     private final UserService userService;
