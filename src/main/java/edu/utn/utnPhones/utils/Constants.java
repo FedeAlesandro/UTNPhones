1daf4545b01e14a8069bfa29a348fee965c7529e
FedeAlesandro <fede.alesandro@gmail.com>
2020-06-20 17:09:45 -0300

    [Feature] Index created, areaCode verification and sp fixed

--- src/main/java/edu/utn/utnPhones/utils/Constants.java
+++ src/main/java/edu/utn/utnPhones/utils/Constants.java
@@ -2,10 +2,11 @@ package edu.utn.utnPhones.utils;
 
 public class Constants {
 
+    public static final String NOT_FOUND_AREA_CODE = "The area code entered is wrong, it doesn't exist";
+
     public static final String NOT_FOUND_BILL = "Bill not found";
 
     public static final String NOT_FOUND_PHONE_LINE = "Phone line not found";
-    public static final String PHONE_LINE_REMOVED = "This phone line is removed";
     public static final String ALREADY_EXISTS_PHONE_LINE = "This phone line already exists";
 
     public static final String USER_NOT_EXIST = "This user doesn't exist";
